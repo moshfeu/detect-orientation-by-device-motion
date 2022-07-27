af6e01197626e8cb7c60c43cfb110647bba415a2
Moshe Feuchtwanger <moshfeu@gmail.com>
2022-07-27 21:51:32 +0300

    implement check

--- App.tsx
+++ App.tsx
@@ -1,10 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { DeviceMotion } from 'expo-sensors';
+import { useEffect, useState } from 'react';
+
 export default function App() {
+  const [orientation, setOrientation] = useState('protrait');
+
+  useEffect(() => {
+    DeviceMotion.addListener(({rotation}) => {
+      const alpha = Math.floor(Math.abs(rotation.alpha));
+      setOrientation(alpha === 1 ? 'landscape' : 'protrait');
+    });
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>{orientation}</Text>
       <StatusBar style="auto" />
     </View>
   );
